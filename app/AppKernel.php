1aad588ab9d519b7aebc90c4d2b918d7ac6b2c39
Sinjonathan <santoni.jonathan@gmail.com>
2015-11-20 16:27:02 +0100

    First version of exposed API ( expose not final Person entity under \people )

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new FOS\HttpCacheBundle\FOSHttpCacheBundle(),
+
+            new \AppBundle\AppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
